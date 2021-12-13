9603e5d3d6168f4d323e33d35270a0d1d583072f
Sonu <sonu@Sonus-MacBook-Air.local>
2021-12-13 22:58:59 +0530

    update the changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome my fiest successfully deployee ciCD on aws server
+          ð
         </p>
         <a
           className="App-link"
